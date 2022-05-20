ff5a61f1e5151436aae3bf53556660c182abc403
Mathias Scherer <mathias@aragon.org>
2022-05-20 16:26:09 +0200

    copy pastes suggestion

--- src/helpers/mimetype.ts
+++ src/helpers/mimetype.ts
@@ -4,33 +4,26 @@ export function isAllowedMimeType(
   file: Buffer,
   allowedMimeTypes: string[]
 ): Promise<boolean> {
-  return new Promise<boolean>((resolve) => {
-    fromBuffer(file)
-      .then((fileType) => {
-        let fileTypeWithDefault = fileType || {mime: "text/plain"};
-        if (fileTypeWithDefault) {
-          for (const type of allowedMimeTypes) {
-            const match = fileTypeWithDefault.mime.match(type);
-            if (match) {
-              resolve(true);
-              return;
-            }
+  return fromBuffer(file)
+    .then((fileType) => {
+      let fileTypeWithDefault = fileType || {mime: "text/plain"};
+      if (fileTypeWithDefault) {
+        for (const type of allowedMimeTypes) {
+          const match = fileTypeWithDefault.mime.match(type);
+          if (match) {
+            return true;
           }
         }
-        if (allowedMimeTypes.includes("application/json")) {
-          try {
-            JSON.parse(file.toString());
-            resolve(true);
-            return;
-          } catch (e) {
-            // ignore
-          }
+      }
+      if (allowedMimeTypes.includes("application/json")) {
+        try {
+          JSON.parse(file.toString());
+          return true;
+        } catch (e) {
+          // ignore
         }
-        resolve(false);
-      })
-      .catch(() => {
-        // return false in case of an error
-        resolve(false);
-      });
-  });
+      }
+      return false;
+    })
+    .catch(() => false);
 }
