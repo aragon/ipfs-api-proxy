424cfb217796c045fba80596b95035b080515fd1
Mathias Scherer <mathias@aragon.org>
2022-12-05 15:13:22 +0100

    adds pin endpoint

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,10 @@ dotenv.config();
 
 import Logger from "./helpers/logger";
 import Express from "express";
-import {addRouteMiddleware} from "./middlewares/add";
-import {proxyRequest} from "./helpers/proxyRequest";
-import {apiAuth} from "./middlewares/apiAuth";
+import { addRouteMiddleware } from "./middlewares/add";
+import { proxyRequest } from "./helpers/proxyRequest";
+import { apiAuth } from "./middlewares/apiAuth";
+import { pinRouteMiddlware } from "./middlewares/pin";
 
 const logger = new Logger("main");
 
@@ -14,9 +15,9 @@ logger.debug("Adding routes");
 const ipfsBackend = process.env.IPFS_BACKEND || "http://127.0.0.1:5001";
 const maxBodySize = parseInt(process.env.MAX_BODY_SIZE || "10485760"); // 1024 * 1024 * 10 as default value
 const allowDirectories = process.env.ALLOW_DIRECTORIES === "true";
-const allowedMimeTypes = process.env.ALLOWED_MIMETYPES?.split(",").map((type) =>
-  type.trim()
-) || [".*"];
+const allowedMimeTypes =
+  process.env.ALLOWED_MIMETYPES?.split(",").map((type) => type.trim()) ||
+  [".*"];
 const allowedApiKeys =
   process.env.ALLOWED_API_KEYS?.split(",").map((key) => key.trim()) || [];
 
@@ -31,13 +32,19 @@ app.post(
   "/api/v0/add",
   apiAuth(allowedApiKeys),
   addRouteMiddleware(maxBodySize, allowDirectories, allowedMimeTypes),
-  proxyRequest(ipfsBackend)
+  proxyRequest(ipfsBackend),
 );
 
 app.post("/api/v0/cat", apiAuth(allowedApiKeys), proxyRequest(ipfsBackend));
 app.post("/api/v0/get", apiAuth(allowedApiKeys), proxyRequest(ipfsBackend));
 
 app.post("/api/v0/id", apiAuth(allowedApiKeys), proxyRequest(ipfsBackend));
+
+app.post(
+  "/api/v0/pin/add",
+  pinRouteMiddlware(maxBodySize, allowedMimeTypes, ipfsBackend),
+  proxyRequest(ipfsBackend),
+);
 
 app.listen(process.env.PORT, () => {
   logger.info(`Server listening on port ${process.env.PORT}`);
---
+++ src/middlewares/pin.ts
@@ -0,0 +1,42 @@
+import axios from "axios";
+import Express from "express";
+import { isAllowedMimeType } from "../helpers/mimetype";
+import Logger from "../helpers/logger";
+
+export function pinRouteMiddlware(
+  maxFileSize: Number,
+  allowedMimeTypes: string[],
+  ipfsBackend: string,
+): Express.Handler {
+  const logger = new Logger("pinRouteMiddleware");
+  return async (
+    req: Express.Request,
+    res: Express.Response,
+    next: Express.NextFunction,
+  ) => {
+    try {
+      const cid = req.query.arg;
+      const response = await axios.post(`${ipfsBackend}/api/v0/cat?arg=${cid}`);
+      const responseBuffer = Buffer.from(response.data);
+      const allowed = await isAllowedMimeType(responseBuffer, allowedMimeTypes);
+      if (!allowed) {
+        res.contentType("text").status(400).send("File type not allowed");
+        return;
+      }
+
+      if (Buffer.from(responseBuffer).byteLength > maxFileSize) {
+        res.contentType("text").status(400).send("File is too large");
+        return;
+      }
+      next();
+    } catch (err: any) {
+      if (
+        err.response.data && err.response.data &&
+        err.response.data.Type === "error"
+      ) {
+        res.contentType("text").status(500).send("Something went wrong");
+      }
+      logger.error(JSON.stringify(err.response.data));
+    }
+  };
+}
