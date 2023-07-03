291f381f476a470e30b6efecb7f8ea8e43ca1ec0
Mathias Scherer <mathias@aragon.org>
2023-07-03 17:32:17 +0200

    fix: mimetype detection for pinning data

--- src/helpers/mimetype.ts
+++ src/helpers/mimetype.ts
@@ -1,28 +1,35 @@
-import {fromBuffer} from "file-type";
+import filetype from "file-type";
+import Logger from "./logger";
 
 export function isAllowedMimeType(
   file: Buffer,
   allowedMimeTypes: string[]
 ): Promise<boolean> {
-  return fromBuffer(file)
+  const logger = new Logger("isAllowedMimeType");
+  return filetype
+    .fromBuffer(file)
     .then((fileType) => {
-      let fileTypeWithDefault = fileType || {mime: "text/plain"};
+      logger.debug(`Got fileType ${fileType}`);
+      let fileTypeWithDefault = fileType || { mime: "text/plain" };
       if (fileTypeWithDefault) {
         for (const type of allowedMimeTypes) {
           const match = fileTypeWithDefault.mime.match(type);
           if (match) {
+            logger.info(`Filetype ${fileType?.mime} allowed`);
             return true;
           }
         }
       }
       if (allowedMimeTypes.includes("application/json")) {
         try {
+          logger.info(`Filetype appcliation/json allowed`);
           JSON.parse(file.toString());
           return true;
         } catch (e) {
           // ignore
         }
       }
+      logger.info(`Filetype ${fileType?.mime} not allowed!`);
       return false;
     })
     .catch(() => false);
--- src/middlewares/pin.ts
+++ src/middlewares/pin.ts
@@ -4,7 +4,7 @@ import { isAllowedMimeType } from "../helpers/mimetype";
 import Logger from "../helpers/logger";
 
 export function pinRouteMiddlware(
-  maxFileSize: Number,
+  maxFileSize: number,
   allowedMimeTypes: string[],
   ipfsBackend: string
 ): Express.Handler {
@@ -16,20 +16,18 @@ export function pinRouteMiddlware(
   ) => {
     try {
       const cid = req.query.arg;
-      const response = await axios.post(`${ipfsBackend}/api/v0/cat?arg=${cid}`);
-      let responseBuffer: Buffer;
-      if (typeof response.data != "string") {
-        responseBuffer = Buffer.from(JSON.stringify(response.data));
-      } else {
-        responseBuffer = Buffer.from(response.data);
-      }
-      const allowed = await isAllowedMimeType(responseBuffer, allowedMimeTypes);
+      const response = await axios.post(
+        `${ipfsBackend}/api/v0/cat?arg=${cid}`,
+        undefined,
+        { responseType: "arraybuffer" }
+      );
+      const allowed = await isAllowedMimeType(response.data, allowedMimeTypes);
       if (!allowed) {
         res.contentType("text").status(400).send("File type not allowed");
         return;
       }
 
-      if (Buffer.from(responseBuffer).byteLength > maxFileSize) {
+      if (Buffer.from(response.data).byteLength > maxFileSize) {
         res.contentType("text").status(400).send("File is too large");
         return;
       }
@@ -45,7 +43,10 @@ export function pinRouteMiddlware(
         return;
       }
       logger.error(JSON.stringify(err));
-      res.contentType("text").status(500).send('Something went wrong! Please try again...');
+      res
+        .contentType("text")
+        .status(500)
+        .send("Something went wrong! Please try again...");
     }
   };
 }
